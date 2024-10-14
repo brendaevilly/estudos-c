58e9d25bf5bcf637bbeb261f07d59dd662c73fbe
Brenda Evilly <165739999+brendaevilly@users.noreply.github.com>
2024-10-13 22:00:03 -0300

    .

--- exercicio_bases-numericas/conversao.c
+++ exercicio_bases-numericas/conversao.c
@@ -5,7 +5,7 @@
 
     int binarioPdecimal (int bin);
     void decimalPbinario (int dec);
-    void binarioFracPdecimal (int binF);
+    void decimalPoctal (int dec);
 
     int main(){
         int opcao, binario, decimal;
@@ -15,8 +15,8 @@
         printf ("====================== MENU ====================\n");
         printf ("# 1 - CONVERTER BINARIO INTEIRO P/ DECIMAL\n");
         printf ("# 2 - CONVERTER DECIMAL INTEIRO P/ BINARIO\n");
-        printf ("# 3 - CONVERTER BINARIO FRACIONADO P/ DECIMAL\n");
-        printf ("# 4 - CONVERTER DECIMAL FRACIONADO P/ INTEIRO\n");
+        printf ("# 3 - CONVERTER DECIMAL PARA OCTAL\n");
+        printf ("# 4 - \n");
         printf ("================================================\n");
         printf ("\n");
         printf ("\n# DIGITE SUA OPCAO: ");
@@ -37,9 +37,9 @@
                 decimalPbinario (decimal);
             break;
             case 3:
-                printf ("\n# digite o valor binario fracionado: ");
-                scanf ("%d", &binario);
-                binarioFracPdecimal (binario);
+                printf ("\n# digite o valor em decimal: ");
+                scanf ("%d", &decimal);
+                decimalPoctal (decimal);
             break;
             case 4:
             break;
@@ -65,24 +65,34 @@
 
     void decimalPbinario (int dec){
         int divisao = 0, auxiliar = dec, i = 0;
-        while (divisao != 1){
-            divisao = dec/2;
-            i++;
-        }
-        int vetor[i];
-        for (int l=0; l<=i; l++){
-            vetor[l] = auxiliar%2;
-            auxiliar = auxiliar/2;
-        }
-        printf ("\n# valor de %d em binario: %d", dec, divisao);
-        for (int l=i-1; l>=0; l--){
-            printf ("%d", vetor[l]);
-        }
+
+        if (dec == 1){
+            printf ("\n# valor de %d em binario: %d", dec, dec);
+        }else if(dec == 0){
+            printf ("\n# valor de %d em binario: %d", dec, dec);
+        }else{
+            while (divisao != 1){ 
+                divisao = dec/2;
+                i++; // 123
+            }
+
+            int vetor[i];
+            for (int l=0; l<=i; l++){
+                vetor[l] = auxiliar%2; // 010
+                auxiliar = auxiliar/2; //521
+            }
+            printf ("\n# valor de %d em binario: %d", dec, divisao);
+            for (int l=i-1; l>=0; l--){
+                printf ("%d", vetor[l]);
+            }
+        } 
+
+        
 
     }
 
-    void binarioFracPdecimal (int binF){
-        
+    void decimalPoctal (int dec){
+     
     }
 
 
